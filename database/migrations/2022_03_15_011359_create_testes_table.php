7ff6ed91b22c5d989e7dbbe0060bb7102d8ce604
roseneidesantos <roseneide2santos@bol.com.br>
2022-03-15 02:10:08 -0300

    Criação de conexão com banco MySQL

---
+++ database/migrations/2022_03_15_011359_create_testes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->codigo();
+            $table->nometipo();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testes');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', function () {
-    return view('acesso');
+    return view('auth/login');
 });
 
+
+Auth::routes();
